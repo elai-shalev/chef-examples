5ae64bccceb2cfe9204f49e4628f367a98615506
Eloy Coto <eloy.coto@acalustra.com>
2025-10-07 11:20:37 +0200

    feat: cleanup

---
+++ Policyfile.rb
@@ -0,0 +1,13 @@
+name 'nginx-multisite-policy'
+
+default_source :supermarket
+
+run_list 'nginx-multisite::default', 'cache::default'
+
+cookbook 'nginx-multisite', path: './cookbooks/nginx-multisite'
+cookbook 'cache', path: './cookbooks/cache'
+cookbook 'nginx', '~> 12.0'
+cookbook 'ssl_certificate', '~> 2.1'
+cookbook 'memcached', '~> 6.0'
+cookbook 'redisio', '~> 7.2.4'
+
